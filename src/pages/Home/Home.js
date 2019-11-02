84c098fb68405299421210e518fc9756826dea62
Redinaldo Gomes <redi.gomes@gmail.com>
2019-11-02 11:15:29 -0300

    Setting home page and iTunes API ENDPONT

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,46 @@
-import React from 'react'
-import './Home.css'
+import React, { useState } from 'react';
+import api from '../../services/api';
+import './Home.css';
+
+import logo from '../../assets/logo.png';
+
 
 export default function Home() {
+    const [search, setSearch] = useState('');
+    const [select, setSelect] = useState('album');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const correctedSearch = search.replace(/\s+/g, ' '); //removing consecutive spaces if there's any
+        const artist = correctedSearch.split(' ').join('+'); //replacing spaces with +
+
+        const ENDPOINT = `/search?term=${artist}&entity=${select}`
+        
+        const response = await api.get(ENDPOINT)
+
+        console.log(response.data);
+        
+
+        
+    }
+
     return (
-        <h1>Home Page</h1>
+        <div className="home-container">
+            <form onSubmit={handleSubmit}>
+            <img src={logo} alt="Music Search" />
+                <div className="search-form">
+                    <select value={select} onChange={e => setSelect(e.target.value)}>
+                        <option value="album">Albums</option>
+                        <option value="song">Songs</option>
+                    </select>
+                    <input
+                        placeholder="Search"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                    />
+                </div>
+            </form>
+        </div>
     )
 }
